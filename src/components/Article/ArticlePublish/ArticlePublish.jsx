d4576ac372e323798d0072789d1cbe22349367f0
caiweijia <614789260@qq.com>
2018-12-26 08:12:47 +0800

    1226早上

--- src/components/Article/ArticlePublish/ArticlePublish.jsx
+++ src/components/Article/ArticlePublish/ArticlePublish.jsx
@@ -1,26 +1,8 @@
 import React, { Component } from 'react'
-import { Editor } from '@tinymce/tinymce-react'
 
 export class ArticlePublish extends Component {
-  handleEditorChange = e => {
-    console.log(e.target.getContent())
-  }
   render() {
-    return (
-      <div className="articlePublice">
-        <Editor
-          initialValue="<p>This is the initial content of the editor</p>"
-          apiKey='iekxjcaq7i59luf3of5fdmxbwkot94dzkun3ngmg30qbmg48'
-          init={{
-            plugins: 'link image code',
-            toolbar:
-              'undo redo | bold italic | alignleft aligncenter alignright | code',
-            language: 'zh_cn'
-          }}
-          onChange={this.handleEditorChange}
-        />
-      </div>
-    )
+    return <div className="articlePublice">qqq</div>
   }
 }
 
